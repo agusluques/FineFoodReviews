09556d3fbb7cd7c62f3ba78e65064d3f43ec4e21
joaquinbf <joaquinbf@live.com>
2016-11-20 04:26:59 -0300

    solucionado algunos problemas

--- hashing_trick.py
+++ hashing_trick.py
@@ -4,20 +4,8 @@ from random import *
 from loadcsv import *
 from clean_text import *
 
-def find_next_prime(n):
-    return find_prime_in_range(n, 2*n)
-
-def find_prime_in_range(a, b):
-    for p in range(a, b):
-        for i in range(2, p):
-            if p % i == 0:
-                break
-        else:
-            return p
-	return None
-
 def gen_fhash(m):
-	p = find_next_prime(m)
+	p = m
 	a = randint(1, p-1)
 	b = randint(0, p-1)
 	fhashint = lambda x: (((a * x + b) % p) % m)
@@ -42,18 +30,19 @@ def hashing_trick(dim, test_name, train_name):
 	test_file = "test_clean.csv"
 	if not isfile(train_file) or not isfile(test_file):
 		clean_text()
-	train = load_csv(train_file, "str")
-	test = load_csv(test_file, "str")
+
+	train = load_csv(train_file, None)
+	test = load_csv(test_file, None)
 	fhash = gen_fhash(dim)	
 	test_tht = []
 	train_tht = []
 	for t in train[1:]:
-		vtext = tovect(fhash, t[3], dim)
+		vtext = tovect(fhash, t[9], dim)
 		train_tht.append(([t[6]] + vtext))
 
 	save_csv(train_tht, train_name)
 	for t in test[1:]:
-		vtext = tovect(fhash, t[2], dim)
+		vtext = tovect(fhash, t[8], dim)
 		test_tht.append([t[0]] + vtext)
 
 	save_csv(test_tht, test_name)
--- loadcsv.py
+++ loadcsv.py
@@ -1,6 +1,12 @@
 from numpy import *
 def load_csv(fname, otype):
-	return genfromtxt(fname, delimiter=",", dtype=otype)
+	if otype != None:
+		return genfromtxt(open(fname, "rb"), comments="\\",delimiter=',', dtype=otype)
+
+	to_read = open(fname, "rb")
+	data = [r.split(',') for r in to_read]
+	to_read.close()
+	return data
 
 def save_csv(data, fname):
 	to_write = open(fname, "w")
